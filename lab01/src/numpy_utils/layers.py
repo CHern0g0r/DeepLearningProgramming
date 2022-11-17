377385bb430174fbbbc6c2c6d194d826fe7f0f85
CHern0g0r <fyodorchernogor@gmail.com>
2022-11-17 23:03:46 +0300

    Finish linear, acts with backwards

--- lab01/src/numpy_utils/layers.py
+++ lab01/src/numpy_utils/layers.py
@@ -59,23 +59,6 @@ class Layer:
         )
 
 
-class SequentialLayer(Layer):
-    def __init__(self, *layers):
-        super().__init__()
-        self.sublayers = {
-            f'l{i}': layer
-            for i, layer in enumerate(layers)
-        }
-
-    def forward(self, X):
-        for _, l in self.sublayers.items():
-            X = l(X)
-        return X
-
-    def backward(self, grad):
-        ...
-
-
 class LinearLayer(Layer):
     def __init__(self, input, output):
         super().__init__()
@@ -84,6 +67,8 @@ class LinearLayer(Layer):
         self.W = self._init(self.output, self.input)
         self.b = self._init(self.output)
         self.inp = None
+        self.dW = None
+        self.db = None
 
     def forward(self, X):
         res = np.dot(X, self.W.T) + self.b
@@ -91,7 +76,9 @@ class LinearLayer(Layer):
         return res
 
     def backward(self, grad):
-        ...
+        self.dW = np.dot(grad.T, self.inp)
+        self.db = np.sum(grad, axis=0)
+        return np.dot(grad, self.W)
 
 
 class ConvLayer(Layer):
@@ -110,7 +97,7 @@ class ConvLayer(Layer):
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        self.dilation = dilation-1
+        self.dilation = dilation - 1
 
         self.b = self._init(
             out_channels,
@@ -124,7 +111,7 @@ class ConvLayer(Layer):
         )
         self.win = None
         self.Xin = None
-        self.db, self.dw, self.dx = None, None, None
+        self.db, self.dw = None, None
 
     def forward(self, X):
         n, c, h, w = X.shape
@@ -152,9 +139,9 @@ class ConvLayer(Layer):
 
         self.db = np.sum(dout, axis=(0, 2, 3))
         self.dw = np.einsum('bihwkl,bohw->oikl', self.win, dout)
-        self.dx = np.einsum('bohwkl,oikl->bihw', dout_windows, rot_kern)
+        dx = np.einsum('bohwkl,oikl->bihw', dout_windows, rot_kern)
 
-        return self.db, self.dw, self.dx
+        return dx
 
     def _get_windows(self, input, output_size):
         if self.dilation != 0:
@@ -188,6 +175,40 @@ class ConvLayer(Layer):
             (batch_str, channel_str, self.stride * kern_h_str,
              self.stride * kern_w_str, kern_h_str, kern_w_str)
         )
+
+
+# Aux layers
+class OneLayer(Layer):
+    def forward(self, *X):
+        return 1
+
+    def backward(self, *grad):
+        return 1
+
+
+class FlattenLayer(Layer):
+    def forward(self, X):
+        return X.reshape(X.shape[0], -1)
+
+    def backward(self, grad):
+        return grad
+
+
+class SequentialLayer(Layer):
+    def __init__(self, *layers):
+        super().__init__()
+        self.sublayers = {
+            f'l{i}': layer
+            for i, layer in enumerate(layers)
+        }
+
+    def forward(self, X):
+        for _, l in self.sublayers.items():
+            X = l(X)
+        return X
+
+    def backward(self, grad):
+        ...
 
 
 # No overfitting Layers
@@ -243,9 +264,9 @@ class SigmoidLayer(Layer):
         self.X = X
         return _sigmoid(X)
 
-    def backward(self, grad):
+    def derivative(self):
         sig = _sigmoid(self.X)
-        return grad * sig * (1 - sig)
+        return sig * (1 - sig)
 
 
 class SiLULayer(Layer):
@@ -255,11 +276,11 @@ class SiLULayer(Layer):
 
     def forward(self, X):
         self.X = X
-        return 1.0 / (1 + np.exp(-1 * X)) * X
+        return 1.0 / (1 + np.exp(-X)) * X
 
     def derivative(self):
         sig = _sigmoid(self.X)
-        deriv = sig(1 + self.X * (1 - sig))
+        deriv = sig * (1 + self.X * (1 - sig))
         return deriv
 
 
@@ -272,9 +293,6 @@ class SoftmaxLayer(Layer):
         return self.old_y * (
             grad - (grad * self.old_y).sum(axis=1)[:, None]
         ) / self.old_y.shape[0]
-
-    # def derivative(self):
-    #     return
 
 
 # Criterion
@@ -293,10 +311,7 @@ class CrossEntropyCost(Layer):
         return self.reduction_fn(sumed)
 
     def backward(self):
-        return np.where(self.old_y == 1, -1/self.old_x, 0)
-
-    # def derivative(self):
-    #     return -y_true/(y_pred + 10**-100)
+        return np.where(self.old_y == 1, -1 / self.old_x, 0)
 
 
 class CrossEntropyLoss(Layer):
@@ -314,13 +329,9 @@ class CrossEntropyLoss(Layer):
         sm = np.exp(X) / np.sum(np.exp(X), axis=1)[:, None]
         out = np.sum(-targets * np.log(sm), axis=1)
 
-        # pred_pr = X[np.arange(len(X)), y]
-        # ls = - np.log(np.exp(pred_pr) / np.sum(np.exp(X), axis=1))
-
-        # self.ls = ls
         self.sm = sm
         self.targets = targets
         return self.reduction_fn(out)
 
-    def backward(self, grad):
+    def backward(self):
         return (self.sm - self.targets) / self.sm.shape[0]
---
+++ lab01/src/numpy_utils/utils.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+def save_grad(lst):
+    def hook(grad):
+        lst.append(grad)
+    return hook
+
+
+def ttn(tensor):
+    return tensor.detach().numpy()
+
+
+def check_comb(torch_layers,
+               numpy_layers,
+               X, label=None,
+               return_grad=False,
+               verbose=True):
+
+    assert (label is not None) >= return_grad
+
+    show = lambda x: None
+    if verbose:
+        show = print
+
+    Xts = [X]
+    Xns = [X.detach().numpy()]
+
+    labeln = None if label is None else label.numpy()
+
+    gradst = []
+    rest = X
+    if return_grad:
+        rest.register_hook(save_grad(gradst))
+    for layer in torch_layers[:-1]:
+        rest = layer(rest)
+        if return_grad:
+            rest.register_hook(save_grad(gradst))
+        Xts += [rest]
+    if label is None:
+        rest = torch_layers[-1](rest)
+    else:
+        rest = torch_layers[-1](rest, label)
+    Xts += [rest]
+
+    resn = Xns[0]
+    for layer in numpy_layers[:-1]:
+        resn = layer(resn)
+        Xns += [resn]
+    if labeln is None:
+        resn = numpy_layers[-1](resn)
+    else:
+        resn = numpy_layers[-1](resn, labeln)
+    Xns += [resn]
+
+    show('Forward is right', np.allclose(ttn(rest), resn))
+    show('but', np.power(ttn(rest) - resn, 2).mean())
+
+    result = (
+        rest,
+        resn,
+        Xts,
+        Xns
+    )
+
+    if return_grad:
+        rest.backward()
+
+        grad = numpy_layers[-1].backward()
+        gradsn = [grad]
+        for lyr in numpy_layers[-2::-1]:
+            grad = lyr.backward(grad)
+            gradsn.append(grad)
+
+        result += (gradst, gradsn)
+
+    return result
