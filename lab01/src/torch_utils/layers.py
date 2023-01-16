476f0cde80ed59598537b39e28bb946ee5999da5
CHern0g0r <fyodorchernogor@gmail.com>
2023-01-16 23:59:22 +0300

    Torch Efficient

--- lab01/src/torch_utils/layers.py
+++ lab01/src/torch_utils/layers.py
@@ -20,11 +20,16 @@ Sameseq = Union[int, tuple[int], list[int]]
 class ConvArgs:
     in_channels: int = field()
     out_channels: int = field()
-    kernel_size: Sameseq = field(1)
+    kernel_size: Sameseq = field(default=1)
     stride: Sameseq = field(default=1)
-    padding: Sameseq = field(default=0)
+    padding: Sameseq = field(default=None)
     dilation: Sameseq = field(default=1)
-    bias: bool = field(default=True)
+    bias: bool = field(default=False)
+    groups: bool = field(default=1)
+
+    def __post_init__(self):
+        if self.padding is None:
+            self.padding = (self.kernel_size - 1) // 2 * self.dilation
 
 
 @dataclass
@@ -35,11 +40,15 @@ class MBconfig:
     input_channels: int = field()
     out_channels: int = field()
     num_layers: int = field()
-    width_mult: float = field()
-    depth_mult: float = field()
+    width_mult: float = field(default=1.0)
+    depth_mult: float = field(default=1.0)
+    sd_prob: float = field(default=0.2)
 
     @staticmethod
-    def _make_divisible(self, v: float, divisor: int, min_value: Optional[int] = None) -> int:
+    def _make_divisible(v: float,
+                        divisor: int,
+                        min_value: Optional[int] = None
+                        ) -> int:
         if min_value is None:
             min_value = divisor
         new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
@@ -48,66 +57,67 @@ class MBconfig:
         return new_v
 
     @staticmethod
-    def adjust_channels(self, channels: int, width_mult: float, min_value: Optional[int] = None) -> int:
-        return self._make_divisible(channels * width_mult, 8, min_value)
+    def adjust_channels(channels: int, width_mult: float,
+                        min_value: Optional[int] = None) -> int:
+        return MBconfig._make_divisible(channels * width_mult, 8, min_value)
 
     @staticmethod
     def adjust_depth(num_layers: int, depth_mult: float):
         return int(ceil(num_layers * depth_mult))
 
     def __post_init__(self):
-        self.input_channels = self.adjust_channels(self.input_channels, self.width_mult)
-        self.out_channels = self.adjust_channels(self.out_channels, self.width_mult)
-        self.num_layers = self.adjust_depth(self.num_layers, self.depth_mult)
+        self.input_channels = self.adjust_channels(
+            self.input_channels, self.width_mult
+        )
+        self.out_channels = self.adjust_channels(
+            self.out_channels, self.width_mult
+        )
+        self.num_layers = self.adjust_depth(
+            self.num_layers, self.depth_mult
+        )
 
 
 class StochDepth(nn.Module):
-    def __init__(self, p: float = 0.2, mode: str = 'row') -> None:
+    def __init__(self, p: float = 0.2) -> None:
         super().__init__()
         self.p = p
-        self.mode = mode
 
-    def _stochastic_depth(input: torch.Tensor, p: float, mode: str, training: bool = True) -> torch.Tensor:
+    def _stochastic_depth(self, X: torch.Tensor, p: float,
+                          training: bool = True) -> torch.Tensor:
         if p < 0.0 or p > 1.0:
-            raise ValueError(f"drop probability has to be between 0 and 1, but got {p}")
-        if mode not in ["batch", "row"]:
-            raise ValueError(f"mode has to be either 'batch' or 'row', but got {mode}")
+            raise ValueError("fuck")
         if not training or p == 0.0:
-            return input
+            return X
 
         survival_rate = 1.0 - p
-        if mode == "row":
-            size = [input.shape[0]] + [1] * (input.ndim - 1)
-        else:
-            size = [1] * input.ndim
-        noise = torch.empty(size, dtype=input.dtype, device=input.device)
+        size = [X.shape[0]] + [1] * (X.ndim - 1)
+        noise = torch.empty(size, dtype=X.dtype, device=X.device)
         noise = noise.bernoulli_(survival_rate)
         if survival_rate > 0.0:
             noise.div_(survival_rate)
-        return input * noise
+        return X * noise
 
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return self._stochastic_depth(input, self.p, self.mode, self.training)
-
-    def __repr__(self) -> str:
-        s = f"{self.__class__.__name__}(p={self.p}, mode={self.mode})"
-        return s
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        return self._stochastic_depth(X, self.p, self.training)
 
 
 class ConvBNSiLU(nn.Module):
     def __init__(self,
                  conv_args: ConvArgs,
-                 activate: bool = True) -> None:
+                 activate: bool = True,
+                 bn: bool = True) -> None:
         super().__init__()
         self.conv = Conv2d(**asdict(conv_args))
-        self.bn = BatchNorm2d(conv_args.out_channels)
-        self.act = SiLU() if activate else None
+        self.bn = (
+            BatchNorm2d(conv_args.out_channels)
+            if bn else nn.Identity()
+        )
+        self.act = SiLU() if activate else nn.Identity()
 
     def forward(self, X):
         X = self.conv(X)
         X = self.bn(X)
-        if self.act is not None:
-            X = self.act(X)
+        X = self.act(X)
         return X
 
 
@@ -118,6 +128,8 @@ class SE(nn.Module):
                  activation: nn.Module = SiLU,
                  scaler: nn.Module = Sigmoid):
         super().__init__()
+
+        # inner_channels = outer_channels // r
         cai = ConvArgs(
             outer_channels,
             inner_channels
@@ -139,6 +151,65 @@ class SE(nn.Module):
         out = self.cout(out)
         out = self.scale(out)
         return out * X
+
+
+class MBConv(nn.Module):
+    def __init__(self, config: MBconfig) -> None:
+        super().__init__()
+
+        self.use_res = (
+            config.stride == 1 and
+            config.input_channels == config.out_channels
+        )
+
+        layers = []
+        expanded_channels = config.adjust_channels(
+            config.input_channels,
+            config.expand_ratio
+        )
+        if expanded_channels != config.input_channels:
+            conv_args = ConvArgs(
+                config.input_channels,
+                expanded_channels
+            )
+            layers.append(
+                ConvBNSiLU(conv_args)
+            )
+
+        conv_args = ConvArgs(
+            expanded_channels,
+            expanded_channels,
+            kernel_size=config.kernel,
+            stride=config.stride,
+            groups=expanded_channels
+        )
+        layers.append(
+            ConvBNSiLU(conv_args)
+        )
+
+        squeeze_channels = max(1, config.input_channels // 4)
+        layers.append(SE(
+            expanded_channels,
+            squeeze_channels
+        ))
+
+        conv_args = ConvArgs(
+            expanded_channels,
+            config.out_channels,
+            kernel_size=1
+        )
+        layers.append(ConvBNSiLU(conv_args))
+
+        self.block = nn.Sequential(*layers)
+        self.stochastic_depth = StochDepth(config.sd_prob)
+        self.out_channels = config.out_channels
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        result = self.block(X)
+        if self.use_res:
+            result = self.stochastic_depth(result)
+            result += X
+        return result
 
 
 class MBConv1(nn.Module):
--- lab01/src/torch_utils/models.py
+++ lab01/src/torch_utils/models.py
@@ -1,13 +1,89 @@
+import copy
 import torch
 
 from torch import nn
+from typing import List
+from torchvision.models import efficientnet_b0
 
-from layers import (
-    MBConv1,
-    MBConv6
+from torch_utils.layers import (
+    MBconfig,
+    MBConv,
+    ConvArgs,
+    ConvBNSiLU
 )
 
 
 class EfficientNet(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 config_list: List[MBconfig],
+                 out_fts: int = 1000,
+                 stochastic_depth_prob: float = 0.2
+                 ):
         super().__init__()
+
+        self.sdp = stochastic_depth_prob
+        self.backbone = self._create_from_configs(config_list)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.clf = nn.Sequential(*[
+            nn.Dropout(p=0.2, inplace=True),
+            nn.Linear(1280, out_fts, bias=True)
+        ])
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.backbone(x)
+
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+
+        x = self.clf(x)
+
+        return x
+
+    def _create_from_configs(self,
+                             config_list: List[MBconfig],
+                             last_channel: int = None) -> nn.Sequential:
+        layers = []
+
+        first_args = ConvArgs(
+            3, config_list[0].input_channels, 3, 2
+        )
+        layers.append(
+            ConvBNSiLU(first_args)
+        )
+
+        total_layers = sum(cfg.num_layers for cfg in config_list)
+        cur_layer = 0
+        for config in config_list:
+            sublayer = []
+            for i in range(config.num_layers):
+                cfg = copy.copy(config)
+                if sublayer:
+                    cfg.input_channels = cfg.out_channels
+                    cfg.stride = 1
+
+                cfg.sd_prob = self.sdp * float(cur_layer) / total_layers
+                block = MBConv(cfg)
+                sublayer.append(block)
+                cur_layer += 1
+            layers.append(nn.Sequential(*sublayer))
+
+        last_in = config_list[-1].out_channels
+        last_out = (
+            last_channel
+            if last_channel is not None
+            else 4 * last_in
+        )
+
+        last_args = ConvArgs(
+            last_in,
+            last_out,
+            kernel_size=1,
+        )
+
+        layers.append(
+            ConvBNSiLU(
+                last_args
+            )
+        )
+
+        return nn.Sequential(*layers)
